ef72c1cf1c673914cef5285351c239007e8b8972
Sisimomo <simon@vallieres.ca>
2023-05-20 01:35:12 -0400

    feat!: the first stable version
    Added GitHub workflow.
    Added Unit Test (Jest).
    Migrated to Typescript.
    Based on https://github.com/tomchen/example-typescript-package.

---
+++ src/comparators.ts
@@ -0,0 +1,130 @@
+import { Function } from './functions';
+import { Item, valuesToValue } from './item';
+
+abstract class Comparator implements Item {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(protected selector: Function | string, protected comparatorKeyWord: string, protected value?: string | number) {}
+
+  toString(): string {
+    if (this.value) {
+      if (typeof this.value === 'number') {
+        return `${this.selector} ${this.comparatorKeyWord} ${this.value}`;
+      } else {
+        return `${this.selector} ${this.comparatorKeyWord} '${this.value}'`;
+      }
+    }
+    return `${this.selector} ${this.comparatorKeyWord}`;
+  }
+}
+
+export class Like extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string, caseInsensitive = false) {
+    super(selector, caseInsensitive ? '~~' : '~', value);
+  }
+}
+
+export class Equal extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, ':', value);
+  }
+}
+
+export class NotEqual extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, '!', value);
+  }
+}
+
+export class Gt extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, '>', value);
+  }
+}
+
+export class Ge extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, '>:', value);
+  }
+}
+
+export class Lt extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, '<', value);
+  }
+}
+export class Le extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, value: string | number) {
+    super(selector, '<:', value);
+  }
+}
+
+export class IsNull extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string) {
+    super(selector, 'is null');
+  }
+}
+
+export class IsNotNull extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string) {
+    super(selector, 'is not null');
+  }
+}
+
+export class IsEmpty extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string) {
+    super(selector, 'is empty');
+  }
+}
+
+export class IsNotEmpty extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string) {
+    super(selector, 'is not empty');
+  }
+}
+
+export class In extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, values: (string | number)[]) {
+    super(selector, 'in', `[${valuesToValue(values)}]`);
+  }
+
+  public toString(): string {
+    return `${this.selector} ${this.comparatorKeyWord} ${this.value}`;
+  }
+}
+
+export class NotIn extends Comparator {
+  // false positive
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  constructor(selector: Function | string, values: (string | number)[]) {
+    super(selector, 'not in', `[${valuesToValue(values)}]`);
+  }
+
+  public toString(): string {
+    return `${this.selector} ${this.comparatorKeyWord} ${this.value}`;
+  }
+}
---
+++ src/operators.ts
@@ -0,0 +1,37 @@
+import { Item } from './item';
+
+abstract class Operator implements Item {
+  protected constructor(protected operatorKeyWord: string, protected items: Item[]) {}
+
+  public toString() {
+    return '(' + this.items.map((item) => item.toString()).join(` ${this.operatorKeyWord} `) + ')';
+  }
+}
+
+export class And extends Operator {
+  public constructor(items: Item[]) {
+    super('and', items);
+  }
+}
+
+export class Or extends Operator {
+  public constructor(items: Item[]) {
+    super('or', items);
+  }
+}
+
+export class Not extends Operator {
+  constructor(item: Item) {
+    super('not', [item]);
+  }
+
+  public toString(): string {
+    return `${this.operatorKeyWord} ${this.items[0].toString()}`;
+  }
+}
+
+export class Exists extends Operator {
+  constructor(item: Item) {
+    super('exists', [item]);
+  }
+}
